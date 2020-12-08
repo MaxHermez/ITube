c3022a69352c6864d895be0725e37c928489e98b
Max <Max.Hermez@hotmail.com>
2020-12-08 21:51:33 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ITube</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
+</head>
+<body>
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+            <button class="navShowHide">Click me</button>
+        </div>
+        <div id="sideNavContainer" style="display: none;">
+
+        </div>
+        <div id="mainSectionContainer">
+            <div id="mainContentContainer">
+                hello everyone
+            </div>
+        </div>
+    </div>
+</body>
+</html>
